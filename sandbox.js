59bca3e2340ae60e282b26d2bd49e7deb6c3b042
Akashiutchiha <christianhonore2003@gmail.com>
2023-08-07 15:16:09 +0100

    Learnt about Arrays and Meals

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,18 @@
-var name = "Chris";
-console.log(name);
+// variable definition
+var myname = "Chris";
+console.log(myname);
 var inputs = document.querySelectorAll("input");
 console.log(inputs);
 inputs.forEach(function (input) {
     console.log(input);
 });
+// Arrays
+var PLangages = ['python', 'javaScript', 'Java'];
+PLangages.push('Dart');
+console.log(PLangages);
+// Objects
+var meals = {
+    chris: "boy",
+    age: 20,
+    height: 1.5
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,6 @@
-let name = "Chris";
-console.log(name);
+// variable definition
+let myname = "Chris";
+console.log(myname);
 
 let inputs = document.querySelectorAll("input");
 console.log(inputs);
@@ -7,3 +8,17 @@ console.log(inputs);
 inputs.forEach((input)=>{
     console.log(input);
 })
+
+// Arrays
+let PLangages = ['python', 'javaScript', 'Java']
+
+PLangages.push('Dart')
+console.log(PLangages)
+
+// Objects
+
+let meals = {
+    chris:"boy",
+    age : 20,
+    height:1.5
+}
