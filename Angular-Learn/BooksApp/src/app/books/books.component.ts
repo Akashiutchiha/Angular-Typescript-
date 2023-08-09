aadfddd12c44a1b6a9dccded52142616cd6f1273
Akashiutchiha <christianhonore2003@gmail.com>
2023-08-09 23:41:50 +0100

    Used services to getBooks data

--- Angular-Learn/BooksApp/src/app/books/books.component.ts
+++ Angular-Learn/BooksApp/src/app/books/books.component.ts
@@ -1,40 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Book } from '../interfaces/Book';
+import { ServicesService } from './services.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit{
 
+  constructor(private bookservice: ServicesService){
+    console.log("constructor")
+  }
+  ngOnInit(): void {
+    this.books = this.bookservice.getbook();
+  }
   // books Data
-  books:Book[] = [
-    {
-      name: "The Pragmatic Programmer",
-      author: "Andrew Hunt and David Thomas",
-      src: "https://m.media-amazon.com/images/I/91WFb-PpoNL._AC_UY218_.jpg",
-      price: 50
-    },
-    {
-      name: "Clean Code",
-      author: "Robert C. Martin",
-      src: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UY218_.jpg",
-      price: 450
-    },
-    {
-      name: "A Programmer's Guide to Computer Science",
-      author: "Dr. William M Springer II",
-      src: "https://m.media-amazon.com/images/I/61fFh20AhQL._AC_UY218_.jpg",
-      price: 200
-    },
-    {
-      name: "A Programmer's Guide to Computer Science",
-      author: "Dr. William M Springer II",
-      src: "https://m.media-amazon.com/images/I/61fFh20AhQL._AC_UY218_.jpg",
-      price: 200
-    },
-  ]
+  books:Book[] = [];
 
   // for controlling display of books
   isShowingBooks:boolean = true;
---
+++ Angular-Learn/BooksApp/src/app/books/services.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+
+  getbook(){
+    return [
+      {
+        name: "The Pragmatic Programmer",
+        author: "Andrew Hunt and David Thomas",
+        src: "https://m.media-amazon.com/images/I/91WFb-PpoNL._AC_UY218_.jpg",
+        price: 50
+      },
+      {
+        name: "Clean Code",
+        author: "Robert C. Martin",
+        src: "https://m.media-amazon.com/images/I/41xShlnTZTL._AC_UY218_.jpg",
+        price: 450
+      },
+      {
+        name: "A Programmer's Guide to Computer Science",
+        author: "Dr. William M Springer II",
+        src: "https://m.media-amazon.com/images/I/61fFh20AhQL._AC_UY218_.jpg",
+        price: 200
+      },
+      {
+        name: "A Programmer's Guide to Computer Science",
+        author: "Dr. William M Springer II",
+        src: "https://m.media-amazon.com/images/I/61fFh20AhQL._AC_UY218_.jpg",
+        price: 200
+      },
+    ]
+  }
+  constructor() { }
+}
